f06c434568e174f538e3ee2d69f304a405c8b7fd
nmaxcom <nmaxcom@gmail.com>
2015-10-07 06:20:15 +0200

    Update gulpfile.js
    Just a bit of aesthetics ;)

--- gulpfile.js
+++ gulpfile.js
@@ -1,60 +1,72 @@
-var gulp = require('gulp'),
-  gutil = require('gulp-util'),
-  sass = require('gulp-sass'),
-  autoprefixer = require('gulp-autoprefixer'),
-  browserSync = require('browser-sync'),
-  imagemin = require('gulp-imagemin'),
-  cache = require('gulp-cache'),
-  rename = require('gulp-rename'),
-  concat = require('gulp-concat'),
-  minifycss = require('gulp-minify-css'),
-  uglify = require('gulp-uglify');
+var gulp     = require('gulp'),
+gutil        = require('gulp-util'),
+sass         = require('gulp-sass'),
+autoprefixer = require('gulp-autoprefixer'),
+browserSync  = require('browser-sync'),
+imagemin     = require('gulp-imagemin'),
+cache        = require('gulp-cache'),
+rename       = require('gulp-rename'),
+concat       = require('gulp-concat'),
+minifycss    = require('gulp-minify-css'),
+uglify       = require('gulp-uglify');
 
 gulp.task('browser-sync', function() {
   browserSync({
-      server: {
-          baseDir: "./"
-      }
+    server: {
+      baseDir: "./"
+    }
   });
 });
 
-gulp.task('styles', function(){
+gulp.task('styles', function() {
   gulp.src(['src/styles/**/*.scss'])
     .pipe(sass())
     .on('error', gutil.log)
     .pipe(autoprefixer('last 2 versions'))
     .on('error', gutil.log)
     .pipe(gulp.dest('dist/styles/'))
-    .pipe(rename({suffix: '.min'}))
+    .pipe(rename({
+      suffix: '.min'
+    }))
     .pipe(minifycss())
     .pipe(gulp.dest('dist/styles/'))
-    .pipe(browserSync.reload({stream:true}))
+    .pipe(browserSync.reload({
+      stream: true
+    }))
 });
 
-gulp.task('images', function(){
+gulp.task('images', function() {
   gulp.src('src/images/**/*')
-    .pipe(cache(imagemin({ optimizationLevel: 3, progressive: true, interlaced: true })))
+    .pipe(cache(imagemin({
+      optimizationLevel: 3,
+      progressive: true,
+      interlaced: true
+    })))
     .pipe(gulp.dest('dist/images/'));
 });
 
-gulp.task('scripts', function(){
+gulp.task('scripts', function() {
   return gulp.src(['src/scripts/plugins/**/*.js', 'src/scripts/app.js'])
     .pipe(gulp.dest('dist/scripts/'))
     .pipe(concat('main.js'))
     .on('error', gutil.log)
     .pipe(gulp.dest('dist/scripts/'))
-    .pipe(rename({suffix: '.min'}))
+    .pipe(rename({
+      suffix: '.min'
+    }))
     .pipe(uglify())
     .on('error', gutil.log)
     .pipe(gulp.dest('dist/scripts/'))
-    .pipe(browserSync.reload({stream:true}))
+    .pipe(browserSync.reload({
+      stream: true
+    }))
 });
 
-gulp.task('bs-reload', function () {
+gulp.task('bs-reload', function() {
   browserSync.reload();
 });
 
-gulp.task('default', ['browser-sync'], function () {
+gulp.task('default', ['browser-sync'], function() {
   gulp.watch("src/styles/**/*.scss", ['styles']);
   gulp.watch("src/scripts/**/*.js", ['scripts']);
   gulp.watch("*.html", ['bs-reload']);
